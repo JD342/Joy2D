94502f3a9393d5eff0e822d3e7270eafe406eab4
Nicola Fiori (JD342) <nicola@jd342.net>
2017-01-12 07:59:38 +0100

    Specify that the script failed abruptly

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,14 @@ const [,,command, ...args] = process.argv;
 require(`./${command}`)(...args).then((code) => {
     process.exit(code);
 }).catch((err) => {
-    console.log(`\n${command[0].toUpperCase()}${command.substr(1)} failed\n`);
+
+    const msg = `
+${command[0].toUpperCase()}${command.substr(1)} failed abruptly
+`;
+
+    console.log(msg);
     console.log(err);
+
     process.exit(-1);
+
 });
